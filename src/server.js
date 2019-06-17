bd46559011ac86836de182aa0f438401114a9943
Yong Kent Tan <tan.yong.kent@amway.com>
2019-06-17 02:10:40 -0400

    test

--- src/server.js
+++ src/server.js
@@ -2,7 +2,6 @@ import serverless from 'serverless-http';
 import express from 'express';
 import bodyParser from 'body-parser';
 
-import auth from './util/auth';
 import health from './health';
 
 const app = express();
@@ -11,8 +10,5 @@ app.use(bodyParser.json({ strict: false }));
 
 // endpoint without authentication
 app.get('/health', health);
-
-app.use(auth);
-// endpont need authentication under here
 
 exports.handler = serverless(app);
