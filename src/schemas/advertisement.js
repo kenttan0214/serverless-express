d463bbb05d63a072386f65fb4a53f4712effefc4
Yong Kent Tan <tan.yong.kent@amway.com>
2019-06-17 02:22:35 -0400

    update

--- src/schemas/advertisement.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-const ADSchema = new mongoose.Schema({
-  email: String,
-  img: String,
-  body: String
-});
-module.exports = mongoose.model('Advertisement', ADSchema);
--- src/util/auth.js
+++
@@ -1,7 +0,0 @@
-const auth = (req, res, next) => {
-  // placeholder for authentication
-  console.log('authentication layer');
-  next();
-};
-
-export default auth;
